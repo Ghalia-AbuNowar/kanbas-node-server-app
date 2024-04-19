0677eb0488ae4592ea5df8b71cf743b5f4dd3d11
Ghalia-AbuNowar <133063833+Ghalia-AbuNowar@users.noreply.github.com>
2024-04-19 15:00:13 -0400

    test3

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -86,26 +86,27 @@ export default function UserRoutes(app) {
             // } else {
             //     res.json(currentUser);
             // }
-        };
-
-
-
-
-
-
-        app.post("/api/users", createUser);
-        app.get("/api/users", findAllUsers);
-        app.get("/api/users/:userId", findUserById);
-        app.put("/api/users/:userId", updateUser);
-        app.delete("/api/users/:userId", deleteUser);
-        app.post("/api/users/signup", signup);
-        app.post("/api/users/signin", signin);
-        app.post("/api/users/signout", signout);
-        app.post("/api/users/profile", profile);
-
-
-
-
-
-
+        }
     }
+
+
+
+
+
+
+    app.post("/api/users", createUser);
+    app.get("/api/users", findAllUsers);
+    app.get("/api/users/:userId", findUserById);
+    app.put("/api/users/:userId", updateUser);
+    app.delete("/api/users/:userId", deleteUser);
+    app.post("/api/users/signup", signup);
+    app.post("/api/users/signin", signin);
+    app.post("/api/users/signout", signout);
+    app.post("/api/users/profile", profile);
+
+
+
+
+
+
+}
